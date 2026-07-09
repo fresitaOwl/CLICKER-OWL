0885b4195f2f3f391a3111e9783257e8bc7a3d3d
fresitaOwl <fresita2614a@gmail.com>
2026-07-09 02:15:32 -0700

    Duplicate cost
    Duplicated the needed cost on upgrades

--- general.js
+++ general.js
@@ -2,6 +2,13 @@ const puntos=document.getElementById("puntos");
 puntos.innerHTML="Food";
 window.addEventListener('keydown', keypress);
 
+var extra=0;
+var Cost=10;
+var price=100;
+var Pricy=250
+var Costs=1000
+var Costo=5000
+
 var contador=0;
 function keypress(event){
     if(event.repeat==true){
@@ -9,13 +16,58 @@ function keypress(event){
     }
     console.log(event.code);
     if(event.code=='Space'){
-        contador+=1;
-        puntos.innerHTML=`Mice=Yummy${contador}`;
+        contador+=1 + extra;
+        puntos.innerHTML=`Food=Yummy${contador}`;
 
     } 
-    if(contador>=10 && event.code=='KeyB'){
-        contador-=10;
-        puntos.innerHTML=`Mice=Yummy${contador}`;
+
+    //Araña
+    if(contador>=Cost && event.code=='KeyB'){
+        contador-=Cost;
+        extra+=1;
+        Cost*=2;
+        spidy.innerHTML=`${Cost}`;
+        puntos.innerHTML=`Insects=Yummy${contador}`;
+        
+    }
+
+    //Ratón
+    if(contador>=price && event.code=='KeyN'){
+        contador-=100;
+         extra+=9;
+          price*=2;
+        micy.innerHTML=`${price}`;
+        puntos.innerHTML=`Mouse=Yummy${contador}`;
+        
+    }
+
+    //Rana
+    if(contador>=Pricy && event.code=='KeyM'){
+        contador-=250;
+         extra+=49;
+          Pricy*=2;
+        froggy.innerHTML=`${Pricy}`;
+        puntos.innerHTML=`Frog=Yummy${contador}`;
+        
+    }
+
+    //Pájaro
+    if(contador>=Costs && event.code=='KeyK'){
+        contador-=1000;
+         extra+=99;
+          Costs*=2;
+        birdy.innerHTML=`${Costs}`;
+        puntos.innerHTML=`Bird=Yummy${contador}`;
+        
+    }
+
+    //Conejo
+    if(contador>=Costo && event.code=='KeyL'){
+        contador-=5000;
+         extra+=300;
+          Costo*=2;
+        bonny.innerHTML=`${Costo}`;
+        puntos.innerHTML=`Rabbit=Yummy${contador}`;
         
     }
 
